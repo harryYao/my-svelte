e5ac9cc3f2d68ffc913d35ba97ec7b079ac59b17
harryYao <yxls0805@163.com>
2021-04-17 23:11:12 +0800

    初始化体验下

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,15 @@
 <script>
+	import Test from './components/test.svelte';
 	export let name;
+	let str = '今天是个好天气！';
+	let src= 'assets/imgs/a.png'
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	<h4>{str}</h4>
+	<img src={src} alt="">
+	<Test></Test>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
@@ -20,6 +26,10 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	h4 {
+		color: rgb(37, 116, 27);
 	}
 
 	@media (min-width: 640px) {
---
+++ src/components/test.svelte
@@ -0,0 +1,14 @@
+<div>
+  <p class="test">{name}</p>
+</div>
+
+<script>
+  let name = 'I\'m a test component!'
+</script>
+
+<style>
+.test {
+  color: rgb(224, 19, 207);
+  font-size: 20px;
+}
+</style>
