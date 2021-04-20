f301a6dec410445458ea015c0050b3dc053e1768
yaoxin <yaoxin1@ztgame.com>
2021-04-20 15:23:50 +0800

    add promise and enent 使用体验

--- learn.md
+++ learn.md
@@ -1,3 +1,9 @@
 进度 ：https://www.sveltejs.cn/tutorial/await-blocks
 
 
+## issues: 404 /service-worker.js
+修改代码后出现如下的错误
+[19:19:08] 404 ─ 6.56ms ─ /service-worker.js
+// https://github.com/sveltejs/template/issues/95
+404 error on service-worker.js after a fresh install
+可能你之前已经在localhot:5000下注册过service-worker, 打开hrome://Serviceworker-Internals/ 找到对应的service-worker 点击 Unregister即可
--- src/components/attr.svelte
+++ src/components/attr.svelte
@@ -1,9 +1,51 @@
 <div>
-  {`My name is ${name}, my lasted version is ${version}, ${desc}. ${$$props.date}`}
+  <p>{`My name is ${name}, my lasted version is ${version}, ${desc}. ${$$props.date}`}</p>
+  <button  on:click="{ handleClick }">获取数据</button>
+  {#await promise}
+    <p>...waiting</p>
+  {:then number}
+    <p>The result is: {JSON.stringify(number)}</p>
+  {:catch error}
+    <p style="color: red">{error.message}</p>
+  {/await}
+</div>
+<div class="mousebox" on:mousemove={handleMousemove}>
+	The mouse position is {m.x} x {m.y}
 </div>
 
 <script>
   export let name;
   export let version;
   export let desc;
+
+  let url = 'http://poetry.apiopen.top/sentences';
+  let promise = getdata();
+  async function getdata() {
+    const res = await fetch(url);
+		const json = await res.json();
+    if (res.ok) {
+      return json;
+    } else {
+      throw new Error(json)
+    }
+  }
+  function handleClick() {
+    promise = getdata();
+  }
+  let m = { x: 0, y: 0 };
+
+	function handleMousemove(event) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
 </script>
+
+<style>
+  .mousebox {
+    text-align: left;
+    margin: auto;
+    width: 600px;
+    height: 300px;
+    border: 1px solid #345678;
+  }
+</style>
